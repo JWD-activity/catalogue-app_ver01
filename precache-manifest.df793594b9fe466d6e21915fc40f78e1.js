c86db35cbea8fe01fa8d8ab66ed5797fa80426e0
Jinok <joseo46@hotmail.com>
2021-11-17 15:45:49 +1100

    Updates

---
+++ precache-manifest.df793594b9fe466d6e21915fc40f78e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9902282554655bddb535eb96687c395",
+    "url": "/catalogue-app_ver01/index.html"
+  },
+  {
+    "revision": "f2f094614b34c9bbc60e",
+    "url": "/catalogue-app_ver01/static/css/2.6dff0e13.chunk.css"
+  },
+  {
+    "revision": "d070c9138065f9427242",
+    "url": "/catalogue-app_ver01/static/css/main.bd31ac0e.chunk.css"
+  },
+  {
+    "revision": "f2f094614b34c9bbc60e",
+    "url": "/catalogue-app_ver01/static/js/2.9951cd82.chunk.js"
+  },
+  {
+    "revision": "435881bb28fec4d4e84baf9dfd194218",
+    "url": "/catalogue-app_ver01/static/js/2.9951cd82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d070c9138065f9427242",
+    "url": "/catalogue-app_ver01/static/js/main.bea3f0c8.chunk.js"
+  },
+  {
+    "revision": "4056291f7bce9f167b3a",
+    "url": "/catalogue-app_ver01/static/js/runtime-main.d58de42c.js"
+  },
+  {
+    "revision": "bad01a8965b8562e2e7447fe04723acd",
+    "url": "/catalogue-app_ver01/static/media/bootstrap-icons.bad01a89.woff"
+  },
+  {
+    "revision": "be57951d47bc6b1635653a38cbdf30fd",
+    "url": "/catalogue-app_ver01/static/media/bootstrap-icons.be57951d.woff2"
+  }
+]);
